b1d2ce0cebc687be67cdd4de8eb2fb3716d03f26
vishaltech <waghmare.vishal.shivaji@gmail.com>
2025-08-02 16:11:19 +0530

    save apis

--- bk/controllers/userController.js
+++ bk/controllers/userController.js
@@ -6,9 +6,9 @@ async function getUserById(req, res) {
   try {
     const [rows] = await db.execute('SELECT * FROM users WHERE id = ?', [id]);
     if (rows.length === 0) {
-      return res.status(404).json({  success: false, message: 'User not found' });
+      return res.status(404).json({ message: 'User not found' });
     }
-    res.status(200).json({  success: true, data : rows[0]});
+    res.status(200).json(rows[0]);
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Server error' });
@@ -21,13 +21,12 @@ async function getUserByName(req, res) {
   try {
     const [rows] = await db.execute('SELECT * FROM users WHERE username = ?', [username]);
     if (rows.length === 0) {
-      return res.status(404).json({  success: false, message: 'User not found' });
+      return res.status(404).json({ message: 'User not found' });
     }
-    // res.status(200).json(rows[0]);
-    res.status(200).json({  success: true, data : rows[0]});
+    res.status(200).json(rows[0]);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ success: false, message: 'Server error' });
+    res.status(500).json({ message: 'Server error' });
   }
 }
 

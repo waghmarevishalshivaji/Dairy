3810337182aa4f0d489071da86c96cf60d51d1e6
vishaltech <waghmare.vishal.shivaji@gmail.com>
2025-08-23 13:18:51 +0530

    save apis

--- bk/config/db.js
+++ bk/config/db.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-  host: "13.233.147.87",
+  host: "13.232.220.5",
   port: 3306,        
   user: "nodeapi",
   password: "Root@123$",

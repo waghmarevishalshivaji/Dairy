87cef0688c9df36d352fd381e2728cfc41a6ea9f
Brahmjot Singh <brahmjots111@gmail.com>
2025-11-22 21:24:03 +0530

    try

--- bk/routes/collectionsRoutes.js
+++ bk/routes/collectionsRoutes.js
@@ -9,11 +9,11 @@ router.get('/', controller.getCollectionBytab);
 router.get('/collectionsummary', controller.getTodaysCollection);
 router.get('/getTodayscollectionfarmer', controller.getTodaysCollectionfarmer);
 router.get('/getTodayscollectionbyfarmer', controller.getTodaysCollectionByFarmer);
+router.get('/billing-period-amount', controller.getBillingPeriodAmount);
 router.get('/:id', controller.getCollectionById);
 router.put('/:id', controller.updateCollection);
 router.delete('/:id', controller.deleteCollection);
 router.post("/update-rates-effective", controller.updateRatesByEffectiveDate);
-router.get('/billing-period-amount', controller.getBillingPeriodAmount);
 
 
 module.exports = router;

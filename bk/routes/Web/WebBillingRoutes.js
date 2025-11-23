e6fd13fd348ad9d1a1ec963859b31df3b0769573
Brahmjot Singh <brahmjots111@gmail.com>
2025-11-23 23:00:15 +0530

    feat: add getBillDetails endpoint to retrieve bill information for a specified period

--- bk/routes/Web/WebBillingRoutes.js
+++ bk/routes/Web/WebBillingRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { createVLCCommission, createVLCTS, upsertBill } = require('../../controllers/Web/WebBillingController');
+const { createVLCCommission, createVLCTS, upsertBill, getBillDetails } = require('../../controllers/Web/WebBillingController');
 
 // POST /api/web/billing/vlc-commission
 router.post('/vlc-commission', createVLCCommission);
@@ -10,5 +10,8 @@ router.post('/vlc-ts', createVLCTS);
 
 // POST /api/web/billing/upsert-bill
 router.post('/upsert-bill', upsertBill);
+
+// GET /api/web/billing/bill-details?dairy_id=1&period_start=2025-11-01&period_end=2025-11-10
+router.get('/bill-details', getBillDetails);
 
 module.exports = router;

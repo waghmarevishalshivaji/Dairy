7e67352dd3c225dd0295e325b4fb4f0ab2b424e2
Brahmjot Singh <brahmjots111@gmail.com>
2025-12-03 20:29:16 +0530

    fix: adjust date filtering to account for timezone conversion

--- bk/controllers/paymentController.js
+++ bk/controllers/paymentController.js
@@ -519,7 +519,7 @@ async function getpayment(req, res) {
       stDate = startDate.trim()
       endDate = endDate.trim()
       if (startDate && endDate) {
-        conditions.push('DATE(date) BETWEEN ? AND ?');
+        conditions.push('DATE(CONVERT_TZ(date, "+00:00", "+05:30")) BETWEEN ? AND ?');
         params.push(startDate, endDate);
       }
     }
@@ -529,13 +529,13 @@ async function getpayment(req, res) {
     if (datefrom && dateto) {
       stDate = datefrom.trim()
       endDate = dateto.trim()
-      conditions.push('DATE(date) BETWEEN ? AND ?');
+      conditions.push('DATE(CONVERT_TZ(date, "+00:00", "+05:30")) BETWEEN ? AND ?');
       params.push(datefrom, dateto);
     } else if (datefrom) {
-      conditions.push('DATE(date) >= ?');
+      conditions.push('DATE(CONVERT_TZ(date, "+00:00", "+05:30")) >= ?');
       params.push(datefrom);
     } else if (dateto) {
-      conditions.push('DATE(date) <= ?');
+      conditions.push('DATE(CONVERT_TZ(date, "+00:00", "+05:30")) <= ?');
       params.push(dateto);
     }
 
